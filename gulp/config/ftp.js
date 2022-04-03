02fa1a39bc2456287725d7fbe3b5ec90db538db3
belasha6 <belasha6@gmail.com>
2022-04-04 01:33:40 +0300

    node

---
+++ gulp/config/ftp.js
@@ -0,0 +1,6 @@
+export let configFTP = {
+	host: "belash.lsh", // Адрес FTP сервера
+	user: "andriyBelash", // Имя пользователя
+	password: "1111", // Пароль
+	parallel: 5 // Кол-во одновременных потоков
+}
